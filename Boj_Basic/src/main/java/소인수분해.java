9b73d0b8d853a9630134b797ba08bda7243b924c
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-01-30 20:32:18 +0900

    11653

---
+++ Boj_Basic/src/main/java/소인수분해.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    
+  static int arr[] = new int[10000000];
+  
+  public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=2; i<=Math.sqrt(n);i++){
+          while(n%i==0){
+            sb.append(i).append('\n');
+            n/=i;
+          }
+        }
+        if(n!=1){
+          sb.append(n);
+        }
+        System.out.println(sb);
+  }
+}
