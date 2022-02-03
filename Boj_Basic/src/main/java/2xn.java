6720c41045b0f7fe832756bdf7dced03e4e950e3
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-02-03 11:51:20 +0900

    11726

---
+++ Boj_Basic/src/main/java/2xn.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if(n == 1 || n == 2){
+            System.out.println(n);
+        }else{
+            int dp[] = new int[n+1];
+            dp[1] = 1;
+            dp[2] = 2;
+            for(int i = 3; i<=n;i++){
+                dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+            }
+            System.out.println(dp[n]);
+        }
+    }
+}
