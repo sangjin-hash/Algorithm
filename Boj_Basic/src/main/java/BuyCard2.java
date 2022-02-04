a31c51488c2b9aa2fe46e669559ea471ec8662bb
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-02-04 16:01:13 +0900

    16194

---
+++ Boj_Basic/src/main/java/BuyCard2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int arr[] = new int[N+1];
+        int dp[];
+
+        for(int i = 1;i<=N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp = arr;
+
+        // DP 점화식
+        for(int i = 2;i<=N;i++){
+            for(int j = 1; j< i; j++){
+                dp[i] = Math.min(dp[i], dp[i-j]+arr[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
