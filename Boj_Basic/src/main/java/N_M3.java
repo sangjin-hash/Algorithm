276f17113eb00707ba1622797940e8882d034b6b
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-02-25 14:43:06 +0900

    15651

---
+++ Boj_Basic/src/main/java/N_M3.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean num[] = new boolean[10];
+    static int res[] = new int[10];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        go(0, n, m);
+        System.out.println(sb.toString());
+    }
+
+    public static void go(int index, int n, int m) {
+        if (index == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(res[i]);
+                if (i != m - 1) sb.append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            res[index] = i;
+            num[i] = true;
+            go(index + 1, n, m);
+            num[i] = false;
+        }
+
+    }
+}
