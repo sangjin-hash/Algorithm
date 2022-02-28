0024591506cdba63bcb7fa7be7b0fd5e0be3dd7b
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-02-28 21:42:27 +0900

    15651

--- Boj_Basic/src/main/java/N_M2.java
+++ Boj_Basic/src/main/java/N_M2.java
@@ -4,47 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean num[] = new boolean[10];
-    static int res[] = new int[10];
     static StringBuilder sb = new StringBuilder();
+    static int[] res = new int[10];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        go(0,n, m);
+        go(1, 0, n, m);
         System.out.println(sb.toString());
     }
 
-    public static void go(int index, int n, int m){
-        if(index == m){
-            boolean flag = true;
-            for(int i = 1; i<m; i++){
-                if(res[i-1] > res[i]){
-                    flag = false;
-                    break;
-                }
+    public static void go(int index,int selected, int n, int m){   // index : 자연수 , selected : 지금까지 선택한 수의 개수
+        if(selected == m){
+            // 수열을 출력
+            for(int i = 0; i<m; i++){
+                sb.append(res[i]);
+                if(i != m-1) sb.append(" ");
             }
-
-            if(flag){
-                for(int i = 0; i < m; i++){
-                    sb.append(res[i]);
-                    if(i != m-1) sb.append(" ");
-                }
-                sb.append('\n');
-                return;
-            }
+            sb.append('\n');
+            return;
         }
 
-        for(int i = 1; i<=n; i++){
-            if(num[i]) continue;
-            res[index] = i;
-            num[i] = true;
-            go(index+1, n, m);
-            num[i] = false;
+        if(index > n){
+            return;
         }
-
+        res[selected] = index;                              // index(자연수)를 선택했으므로 결과 배열에 넣는다.
+        go(index+1, selected+1, n, m);         // index(자연수)를 하나 증가 -> 선택한 개수(selected) 한개 증가
+        res[selected] = 0;                                  // index(자연수)를 결과에 추가 x
+        go(index+1, selected, n, m);                  //  index(자연수)를 하나 증가시키고, 선택한 개수를 한 개 줄인다.
     }
 }
