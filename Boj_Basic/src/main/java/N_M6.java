3e2caaf671bffdef63392b8ebba5224b0d7168bf
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-03-01 16:24:38 +0900

    15655

--- Boj_Basic/src/main/java/N_M6.java
+++ Boj_Basic/src/main/java/N_M6.java
@@ -1,57 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+public class App {
     static StringBuilder sb = new StringBuilder();
-    static int res[] = new int[10];     // 결과를 저장하는 배열
-    static boolean isUse[] = new boolean[10001];       // 중복 허용 x
+    static int[] res = new int[10];
+  
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int num[] = new int[n];
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i<n; i++){
-            num[i] = Integer.parseInt(st1.nextToken());
-        }
-
-        Arrays.sort(num);
-        go(0, n, m, num);
-        System.out.println(sb.toString());
+      StringTokenizer st1 = new StringTokenizer(br.readLine()," ");
+      int num[] = new int[n];
+      for(int i=0;i<n;i++){
+        num[i] = Integer.parseInt(st1.nextToken());
+      }
+      Arrays.sort(num);
+      go(0,0,n,m,num);
+    
+      System.out.println(sb.toString());
     }
 
-    public static void go(int index, int n, int m, int[] num){
-        if(index == m){
-            boolean flag = true;
-            for(int i = 1; i<m; i++){
-                if(res[i-1] > res[i]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                for(int i = 0; i<m; i++){
-                    sb.append(res[i]);
-                    if(i != m-1) sb.append(" ");
-                }
-                sb.append('\n');
-            }
-            return;
-        }
+    public static void go(int index, int selected, int n, int m, int[] num){
+      if(selected==m){
+         for(int i=0;i<m;i++){
+           sb.append(res[i]);
+           if(i!=m-1) sb.append(" ");
+         }
+        sb.append('\n');
+        return;
+      }
 
-        for(int i = 0; i<n; i++){
-            if(isUse[num[i]]) continue;
-            res[index] = num[i];
-            isUse[num[i]] = true;
-            go(index+1, n, m, num);
-            isUse[num[i]] = false;
-        }
+      if(index>n-1){
+        return;
+      }
+      
+      
+      res[selected]=num[index];
+      go(index+1, selected+1, n, m, num);
+      res[selected] = 0;
+      go(index+1, selected,n,m,num);
+    
     }
 }
