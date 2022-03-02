77d09ea7914e9a54e4aefe0dcc37850d93648529
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-03-02 23:37:36 +0900

    15657

--- Boj_Basic/src/main/java/N_M8.java
+++ Boj_Basic/src/main/java/N_M8.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
@@ -11,44 +8,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        int[] num = new int[n];
-        for(int i = 0 ; i < n; i++){
-            num[i] = Integer.parseInt(st1.nextToken());
+        int arr[] = new int[n];
+        for(int i = 0; i<n;i++){
+            arr[i] = Integer.parseInt(st1.nextToken());
         }
-
-        Arrays.sort(num);
-        go(0, n, m, num);
+        Arrays.sort(arr);
+        go(0,0, n, m, arr);
         System.out.println(sb.toString());
     }
 
-    private static void go(int index, int n, int m, int[] num) {
-        if(index == m){
-            boolean flag = true;
-            for(int i = 1; i<m; i++){
-                if(res[i-1] > res[i]){
-                    flag = false;
-                    break;
-                }
+    public static void go(int index, int selected, int n, int m, int[] num){
+        if(selected == m){
+            //수열 출력
+            for(int i = 0; i < m ; i++){
+                sb.append(res[i]);
+                if(i != m-1) sb.append(" ");
             }
-
-            if(flag){
-                for(int i = 0 ; i< m; i++){
-                    sb.append(res[i]);
-                    if(i != m-1) sb.append(" ");
-                }
-                sb.append('\n');
-            }
+            sb.append('\n');
             return;
         }
 
-        for(int i = 0; i<n; i++){
-            res[index] = num[i];
-            go(index+1, n, m, num);
-        }
+        if(index > n-1) return;
+
+        res[selected] = num[index];
+        go(index, selected+1, n, m, num);
+        res[selected] = 0;
+        go(index+1, selected, n, m, num);
     }
+
 }
