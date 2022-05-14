3fe232f387e470bc8ff8100d1d6aad26da7a0d7c
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-05-14 16:45:04 +0900

    1158

---
+++ Boj_Basic/오답노트/Josephus.java
@@ -0,0 +1,37 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1; i<=n; i++){
+            q.offer(i);
+        }
+        sb.append("<");
+
+
+        while(q.size() != 1){
+            for(int i = 0; i<m-1; i++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll()).append(">");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
+
+
+
+
