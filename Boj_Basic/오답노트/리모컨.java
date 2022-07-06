1a4c853bb0a787adda4e687001b96f50266d6af0
sangjin-hash <77181865+sangjin-hash@users.noreply.github.com>
2022-07-06 17:51:02 +0900

    1107

---
+++ Boj_Basic/오답노트/리모컨.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				int n = Integer.parseInt(br.readLine());
+				int m = Integer.parseInt(br.readLine());
+                boolean broken[] = new boolean[10];
+                if(m != 0){
+                    StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
+                    for(int i = 0; i<m; i++){
+						broken[Integer.parseInt(st.nextToken())] = true; 
+				    }
+                }
+                       
+				int result = Math.abs(n - 100);
+				for(int i = 0; i<=999999; i++){
+						String str = String.valueOf(i);
+						int len = str.length();
+						boolean flag = true;
+
+						for(int j = 0; j<len; j++){
+								if(broken[str.charAt(j)-'0']){
+									flag = false;
+									break;
+								}
+						}
+				
+						if(flag){
+								result = Math.min(result, Math.abs(n-i)+len);
+						}
+				}
+				System.out.println(result);
+		}
+}
